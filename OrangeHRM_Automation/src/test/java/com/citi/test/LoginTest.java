08926c6388cf2d9b2602b1d4bccf5b382fcc0b88
naveenramesh <naveen.ramesh0@gmail.com>
2022-10-19 18:25:32 +0530

    From Eclipse.

--- OrangeHRM_Automation/src/test/java/com/citi/test/LoginTest.java
+++ OrangeHRM_Automation/src/test/java/com/citi/test/LoginTest.java
@@ -12,11 +12,11 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.citi.base.AutomationWrapper;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoginTest {
-
-	WebDriver driver;
+public class LoginTest extends AutomationWrapper {
 
 	@BeforeMethod
 	public void setup() {
