5642010df579a7f3e340647a50ddeb013362f354
naveenramesh <naveen.ramesh0@gmail.com>
2022-10-19 17:31:47 +0530

    Signed-off-by: naveenramesh <naveen.ramesh0@gmail.com>

--- OrangeHRM_Automation/src/test/java/com/citi/test/LoginTest.java
+++ OrangeHRM_Automation/src/test/java/com/citi/test/LoginTest.java
@@ -8,29 +8,54 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
 
-	@Test
-	public void validCredentialTest() {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup() {
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
+
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-		
+	}
+
+	@AfterMethod
+	public void conclude() {
+		driver.quit();
+	}
+
+	@Test
+	public void validCredentialTest() {
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.cssSelector("button[type='submit']")).click();
-		
+
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Admin']")));
-		
+
 		String actualvalue = driver.findElement(By.xpath("//h5[text()='Employee Information']")).getText();
 		Assert.assertEquals(actualvalue, "Employee Information");
+	}
+
+	@Test
+	public void invalidCredentialTest() {
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin12");
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h5[text()='Login']")));
+
+		String actualvalue = driver.findElement(By.xpath("//p[text()='Invalid credentials']")).getText();
+		Assert.assertEquals(actualvalue, "Invalid credentials");
 	}
 }
