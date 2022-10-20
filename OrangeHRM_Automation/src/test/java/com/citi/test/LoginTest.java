dd40c50f92a6aa065e085eac3fe0599195ee012b
naveenramesh <naveen.ramesh0@gmail.com>
2022-10-20 15:36:34 +0530

    DataUtils

--- OrangeHRM_Automation/src/test/java/com/citi/test/LoginTest.java
+++ OrangeHRM_Automation/src/test/java/com/citi/test/LoginTest.java
@@ -10,28 +10,17 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.citi.base.AutomationWrapper;
+import com.citi.utilities.DataUtils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest extends AutomationWrapper {
 
-	@BeforeMethod
-	public void setup() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-	}
-
-	@AfterMethod
-	public void conclude() {
-		driver.quit();
-	}
+	
 
 	@Test
 	public void validCredentialTest() {
@@ -46,16 +35,16 @@ public class LoginTest extends AutomationWrapper {
 		Assert.assertEquals(actualvalue, "Employee Information");
 	}
 
-	@Test
-	public void invalidCredentialTest() {
-		driver.findElement(By.name("username")).sendKeys("Admin");
-		driver.findElement(By.name("password")).sendKeys("admin12");
+	@Test(dataProvider = "invalidData",dataProviderClass = DataUtils.class)
+	public void invalidCredentialTest(String username, String password, String expected) {
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
 		driver.findElement(By.cssSelector("button[type='submit']")).click();
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h5[text()='Login']")));
 
 		String actualvalue = driver.findElement(By.xpath("//p[text()='Invalid credentials']")).getText();
-		Assert.assertEquals(actualvalue, "Invalid credentials");
+		Assert.assertEquals(actualvalue, expected);
 	}
 }
